bd7c579ad37c91e34c18ed8eb12be538d0a52340
caradulay <caradulay@gmail.com>
2024-10-07 13:08:31 +0800

    feat: added weather api

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class WeatherController extends Controller
 {
-    //
+    public function index(Request $request){
+        $city = $request->input('city', 'Mandaluyong City');
+
+        $apiKey = env('WEATHER_API_KEY');
+        $client = new Client();
+
+        try {
+            $response = $client->get("http://api.weatherapi.com/v1/current.json?key={$apiKey}&q={$city}&aqi=no");
+            $weatherData = json_decode($response->getBody()->getContents());
+
+            return view('weather', ['weather' => $weatherData]);
+
+        } catch (\Exception $e) {
+            return view('weather', ['error' => 'City not found or API error.']);
+        }
+    }
 }
